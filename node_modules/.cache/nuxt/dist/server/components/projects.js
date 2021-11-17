f48f74747b7d9ba3b8a68a14f38c95f470a4db8a
jun-uen0 <hello@jaysan.io>
2021-11-17 23:46:50 +0900

    fixed a bug

---
+++ node_modules/.cache/nuxt/dist/server/components/projects.js
@@ -0,0 +1,133 @@
+exports.ids = [2];
+exports.modules = {
+
+/***/ 48:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./components/Projects.vue?vue&type=template&id=0d7bf8b4&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"wrapper-small my-5"},[_vm._ssrNode("<div class=\"project-card md:flex mt-8\"><div class=\"img max-w-lg md:max-w-sm  mx-auto\"><img"+(_vm._ssrAttr("src",__webpack_require__(49)))+" alt class=\"rounded-xl\"></div> <div class=\"flex flex-col justify-between max-w-lg mx-auto\"><div class=\"txt md:px-5 lg:px-0\"><h2 class=\"text-xl font-semibold text-gray-800\">Free Translate Web API using Google Apps Script</h2> <p class=\"text-base text-gray-700 my-3\">Nowadays, you can translate text files into any language. GCP, Bing Microsoft Translator, and so on. However, it costs a lot. You can use them freely until the limit.\nAlthough, someone figured out that you can use GCP translate for free. Google Apps Script has its original methods to use Google API, such as Maps API, YouTube API, and Translate API. They are all free. Each method has limitations. It would just stop. You don't need to register your credit card.\nIn this project, you can create your own translate web API. See source code and try it.</p></div> <div><div class=\"md:px-5 lg:px-0 py-3\"><a href=\"https://script.google.com/d/1Si-r4fWsbnq_ltFKlU9dbLxLLJOUtEs8JW51vYUtsupcOhgXjySQbFQt/edit?usp=sharing\" target=\"_blank\" class=\"bg-indigo-600 hover:bg-indigo-700 px-3 py-2 text-sm text-gray-200 font-semibold font-medium rounded-full\">See Project</a> <a href=\"https://github.com/jun-uen0/appsScript-free-translation\" target=\"_blank\" class=\"ml-6 text-indigo-600 hover:underline\">Source Code</a></div></div></div></div> <div class=\"project-card md:flex mt-16\"><div class=\"img max-w-lg md:max-w-sm  mx-auto\"><img"+(_vm._ssrAttr("src",__webpack_require__(50)))+" alt class=\"rounded-xl\"></div> <div class=\"flex flex-col justify-between max-w-lg mx-auto\"><div class=\"txt md:px-5 lg:px-0\"><h2 class=\"text-xl font-semibold text-gray-800\">Video chat app (Text and Translate) </h2> <p class=\"text-base text-gray-700 my-3\">I created a video app that interprets a speaker's language into the listener's language. It's using Google Cloud Translate, but it's completely free. It's because I use Google Translate App as one of the Backend systems that translates the text.</p></div> <div><div class=\"md:px-5 lg:px-0 py-3\"><a href=\"https://video-chat-subtitle.firebaseapp.com/main.html\" target=\"_blank\" class=\"bg-indigo-600 hover:bg-indigo-700 px-3 py-2 text-sm text-gray-200 font-semibold rounded-full\">See Project</a> <a href=\"https://github.com/jun-uen0/react-interpreter-video-app/\" target=\"_blank\" class=\"ml-6 text-indigo-600 hover:underline\">Source Code</a></div></div></div></div> <div class=\"project-card md:flex mt-16\"><div class=\"img max-w-lg md:max-w-sm  mx-auto\"><img"+(_vm._ssrAttr("src",__webpack_require__(51)))+" alt class=\"rounded-xl\"></div> <div class=\"flex flex-col justify-between max-w-lg mx-auto\"><div class=\"txt md:px-5 lg:px-0\"><h2 class=\"text-xl font-semibold text-gray-800\">Smart Contract App using Nuxt.js</h2> <p class=\"text-base text-gray-700 my-3\">You can make a smart contract with Ethereum and connect to your Metamask account. The framework is Nuxt.js. You can create your own chian with AWS.</p></div> <div><div class=\"md:px-5 lg:px-0 py-3\"><a href=\"https://nuxt-blockchain.pages.dev/\" target=\"_blank\" class=\"bg-indigo-600 hover:bg-indigo-700 px-3 py-2 text-sm text-gray-200 font-semibold rounded-full\">See Project</a> <a href=\"https://github.com/jun-uen0/nuxt-blockchain\" target=\"_blank\" class=\"ml-6 text-indigo-600 hover:underline\">Source Code</a></div></div></div></div>")])}
+var staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./components/Projects.vue?vue&type=template&id=0d7bf8b4&
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./components/Projects.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ var Projectsvue_type_script_lang_js_ = ({});
+// CONCATENATED MODULE: ./components/Projects.vue?vue&type=script&lang=js&
+ /* harmony default export */ var components_Projectsvue_type_script_lang_js_ = (Projectsvue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(2);
+
+// CONCATENATED MODULE: ./components/Projects.vue
+
+
+
+function injectStyles (context) {
+  
+  
+}
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  components_Projectsvue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  injectStyles,
+  null,
+  "c32801f2"
+  
+)
+
+/* harmony default export */ var Projects = __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ 49:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "img/project-img-2.e79ef6b.png";
+
+/***/ }),
+
+/***/ 50:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "img/project-img-3.5a1545d.png";
+
+/***/ }),
+
+/***/ 51:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "img/project-img-5.e5ebe3a.png";
+
+/***/ })
+
+};;
+//# sourceMappingURL=projects.js.map
