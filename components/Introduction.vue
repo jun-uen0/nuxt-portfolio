6abef5cc04e084a7b5a1f86d10eae35a6a76cb96
jun-uen0 <hello@jaysan.io>
2021-11-24 19:47:50 +0900

    changed description

--- components/Introduction.vue
+++ components/Introduction.vue
@@ -3,7 +3,7 @@
       <h3 class="text-xl md:text-2xl lg:text-3xl font-semibold text-gray-800"><span class="font-normal text-gray-700"> Hello, I am </span>Jun Ueno</h3> 
       <h2 class="text-2xl md:text-3xl lg:text-4xl font-bold text-primary">I am a Web Software Developer</h2>
       <h4 class="text-lg lg:text-xl font-semibold">I build websites and web apps</h4>
-      <p class="text-md lg:text-lg">I just finished studying at a coding & business boot camp in Tokyo, to improve my comprehension of web development. Now I feel that I'm really fascinated by learning programming and coding so that for more pursuing my curiosity, I'm researching some universities where have CS courses.</p>
+      <p class="text-md lg:text-lg">Currently looking for a job. One of my dream is becoming a blockchain developer. Work hard, study hard.</p>
   </div>
 </template>
 
