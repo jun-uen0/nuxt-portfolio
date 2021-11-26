7bb6d827208a5e3da32f5dd89806c7f4ad364a39
jun-uen0 <hi@jaysan.io>
2021-11-26 12:51:17 +0900

    delete cached files

--- README.md
+++
@@ -1,6 +0,0 @@
-# nuxt-portfolio
-Portfolio site using Nuxt.js<br>
-https://nuxt-portfolio-3at.pages.dev
-
-- Hosting : Cloudflare / Netlify
-- CSS Framework : Tailwind
--- dist/_nuxt/static/1637160369/credits/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637160369",layout:"default",error:{message:"404 page not found",statusCode:500},serverRendered:false,routePath:"\u002Fcredits",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:null},content:{dbHash:"6446c8f0"}}};
