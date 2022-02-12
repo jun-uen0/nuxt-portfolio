c1ce232f4e3bdb7069e762f7da3adf583a126946
jun-uen0 <hello@junueno.dev>
2022-02-12 22:02:12 +0900

    hide url

--- components/Projects.vue
+++ components/Projects.vue
@@ -12,7 +12,7 @@
           </div>
         <div>
           <div class="md:px-5 lg:px-0 py-3">
-            <a href="https://script.google.com/d/1Si-r4fWsbnq_ltFKlU9dbLxLLJOUtEs8JW51vYUtsupcOhgXjySQbFQt/edit?usp=sharing" target="_blank" class="bg-indigo-600 hover:bg-indigo-700 px-3 py-2 text-sm text-gray-200 font-semibold rounded-full">{{ $t('see') }}</a>
+            <!-- <a href="https://script.google.com/d/1Si-r4fWsbnq_ltFKlU9dbLxLLJOUtEs8JW51vYUtsupcOhgXjySQbFQt/edit?usp=sharing" target="_blank" class="bg-indigo-600 hover:bg-indigo-700 px-3 py-2 text-sm text-gray-200 font-semibold rounded-full">{{ $t('see') }}</a> -->
             <a href="https://github.com/jun-uen0/appsScript-free-translation" target="_blank" class="ml-6 text-indigo-600 hover:underline">{{ $t('code') }}</a>
           </div>
         </div>
@@ -30,7 +30,7 @@
         </div>
         <div>
           <div class="md:px-5 lg:px-0 py-3">
-            <a href="https://video-chat-subtitle.firebaseapp.com/main.html" target="_blank" class="bg-indigo-600 hover:bg-indigo-700 px-3 py-2 text-sm text-gray-200 font-semibold rounded-full">{{ $t('see') }}</a>
+            <!-- <a href="https://video-chat-subtitle.firebaseapp.com/main.html" target="_blank" class="bg-indigo-600 hover:bg-indigo-700 px-3 py-2 text-sm text-gray-200 font-semibold rounded-full">{{ $t('see') }}</a> -->
             <a href="https://github.com/jun-uen0/react-interpreter-video-app/" target="_blank" class="ml-6 text-indigo-600 hover:underline">{{ $t('code') }}</a>
           </div>
         </div>
